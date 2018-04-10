5cafa564286f1bfe9b0e3cdadc7be9680f00174c
sabhsn98 <37111915+sabhsn98@users.noreply.github.com>
2018-04-10 04:56:23 +0100

    Update TicketMachine.cs

--- Car Park Simulator Student Version/CarParkSimulator/TicketMachine.cs
+++ Car Park Simulator Student Version/CarParkSimulator/TicketMachine.cs
@@ -6,6 +6,41 @@ using System.Text;
 namespace CarParkSimulator
 {
     class TicketMachine
+        private string displaymessage
+        
     {
+    
+      //public void AssignCarPark()
+        //{
+           //needs main carpark class to function
+      //  }
+
+    
+    
+    
+      public void PrintTicket()
+        {
+            message = "Thank you, enjoy your stay.";
+            //Ticket Dispensing
+            //add it to the to list of active tickets
+     
+        }
+          public void CarArrived()
+        {
+           displaymessage = "Please press to get a ticket.";
+        }
+
+        public void ClearMessage()
+        {
+            displaymessage = string.Empty; //clear the meassage on the ticket machine
+        }
+        
+       public string displaymessage
+       {
+          return displaymessage;
+       
+       }
+
+    
     }
 }
