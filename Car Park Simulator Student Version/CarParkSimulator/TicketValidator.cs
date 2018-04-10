8adb82010dbff888ff906d67435dd0bcab9ff594
sabhsn98 <37111915+sabhsn98@users.noreply.github.com>
2018-04-10 05:02:44 +0100

    Update TicketValidator.cs

--- Car Park Simulator Student Version/CarParkSimulator/TicketValidator.cs
+++ Car Park Simulator Student Version/CarParkSimulator/TicketValidator.cs
@@ -6,6 +6,27 @@ using System.Text;
 namespace CarParkSimulator
 {
     class TicketValidator
+        
     {
+        private string displaymessage;
+        
+        
+        public void ClearMessage()
+        {
+           displaymessage = string.empty();
+            
+        }
+        
+        public void CarArrived()
+        {
+            displaymessage = "Please insert your ticket.";
+        }
+        
+        public void TicketEntered()
+        {
+            displaymessage = "Thank you, drive safely.";
+            //call to validate the ticket
+            //remvove it from activetickets. 
+        
     }
 }
