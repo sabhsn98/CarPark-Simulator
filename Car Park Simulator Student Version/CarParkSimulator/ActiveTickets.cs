6dc035efe5785ab5d6e23ea07ccffeaf03796a4b
sabhsn98 <37111915+sabhsn98@users.noreply.github.com>
2018-04-10 04:49:23 +0100

    Completed.

--- Car Park Simulator Student Version/CarParkSimulator/ActiveTickets.cs
+++ Car Park Simulator Student Version/CarParkSimulator/ActiveTickets.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace CarParkSimulator
 {
+    
+    public List<Ticket> activetickets = new List<Ticket>(); // This is all thats needed, just invoke it from it the main class. 
+    
     class ActiveTickets
     {
     }
